32079cfd46d599622350b5bf5cebfd2c16be65f4
Conor-20105865 <147147702+Conor-20105865@users.noreply.github.com>
2024-11-11 16:18:30 +0000

    initial commit
    YAY

---
+++ Algo_Dat_ASGN_2/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.algo_dat_asgn_2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.algo_dat_asgn_2 to javafx.fxml;
+    exports com.example.algo_dat_asgn_2;
+}
