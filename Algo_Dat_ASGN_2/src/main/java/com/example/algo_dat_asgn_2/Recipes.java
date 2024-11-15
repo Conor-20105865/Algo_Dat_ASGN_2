d863c823b23180ce371d547bceed0e5ada801ed1
Dylan <20106847@mail.wit.ie>
2024-11-15 13:28:28 +0000

    Java amirite?

--- Algo_Dat_ASGN_2/src/main/java/com/example/algo_dat_asgn_2/Recipes.java
+++ Algo_Dat_ASGN_2/src/main/java/com/example/algo_dat_asgn_2/Recipes.java
@@ -1,4 +1,8 @@
 package com.example.algo_dat_asgn_2;
 
-public class Recipes {
+public class Recipes extends Ingredients {
+
+    public Recipes(String iName, String description) {
+        super(iName, description);
+    }
 }
