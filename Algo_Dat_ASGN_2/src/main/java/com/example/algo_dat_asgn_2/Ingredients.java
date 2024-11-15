7897b557a23d9cdde300f9dc8e7d7965adb59b95
Dylan <20106847@mail.wit.ie>
2024-11-15 13:14:07 +0000

    Amen brother

--- Algo_Dat_ASGN_2/src/main/java/com/example/algo_dat_asgn_2/Ingredients.java
+++ Algo_Dat_ASGN_2/src/main/java/com/example/algo_dat_asgn_2/Ingredients.java
@@ -1,4 +1,27 @@
 package com.example.algo_dat_asgn_2;
 
-public class Ingredients {
+public class Ingredients  {
+    String iName;
+    String description;
+
+    public String getName() {
+        return iName;
+    }
+
+    public void setName(String iName) {
+        this.iName = iName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Ingredients(String iName, String description)  {
+        this.iName = iName;
+        this.description = description;
+    }
 }
