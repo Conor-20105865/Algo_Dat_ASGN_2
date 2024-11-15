a2f573642144c7fee9036e291e2a53de223202d6
Dylan <20106847@mail.wit.ie>
2024-11-15 13:28:34 +0000

    Java amirite?

--- Algo_Dat_ASGN_2/src/main/java/com/example/algo_dat_asgn_2/placeholder_java.java
+++ Algo_Dat_ASGN_2/src/main/java/com/example/algo_dat_asgn_2/placeholder_java.java
@@ -1,5 +1,5 @@
 package com.example.algo_dat_asgn_2;
 
 public class placeholder_java {
-    //Conor is mega gay like super mega ultra gay slay queen looksmax gatekeep
+    //Conors kinda acting freaky wth
 }
