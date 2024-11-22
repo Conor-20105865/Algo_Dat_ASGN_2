17b98dba9fcfe4c6b42604f293d0cf2a4ca55412
Dylan Conway <147723183+Dylan-618cmd@users.noreply.github.com>
2024-11-22 11:57:56 +0000

    hash browns are fierce nice

--- Algo_Dat_ASGN_2/src/main/java/com/example/algo_dat_asgn_2/HashTable.java
+++ Algo_Dat_ASGN_2/src/main/java/com/example/algo_dat_asgn_2/HashTable.java
@@ -2,7 +2,27 @@ package com.example.algo_dat_asgn_2;
 
 public class HashTable<K, V> {
     private static final int INTIAL_CAPACITY = 16;
-    private static final double LOAD_FACTOR =- 0.75;
+    private static final double LOAD_FACTOR = -0.75;
 
     private Entry<K, V>[] table;
-}   private int size;
+    private int size;
+
+    public HashTable() {
+        table = new Entry[INTIAL_CAPACITY];
+        size = 0;
+    }
+
+    private static class Entry<K, V> {
+        K key;
+        V value;
+
+        Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private int hash(K key) {
+        return (key.hashCode() % 10);
+    }
+}
